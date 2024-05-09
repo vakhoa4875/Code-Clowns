70415e8e9e17bfe427ed212feebb062b8e91d8e3
khoa <128579628+vakhoa4875@users.noreply.github.com>
2024-05-10 00:42:49 +0700

    demo planny project

---
+++ planny/src/main/java/codeclowns/planny/planny/PlannyApplication.java
@@ -0,0 +1,13 @@
+package codeclowns.planny.planny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannyApplication.class, args);
+	}
+
+}
---
+++ planny/src/test/java/codeclowns/planny/planny/PlannyApplicationTests.java
@@ -0,0 +1,13 @@
+package codeclowns.planny.planny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
