b6304bbee51b4644490eadd72f0e727eb41dd56e
khoa <128579628+vakhoa4875@users.noreply.github.com>
2024-05-13 06:52:57 +0700

    lab2_commit02_Khoa: Created doPostArchiveLoginCredentials API

--- labs/src/main/java/codeclowns/java5/labs/lab2/Api/HomeApi.java
+++ labs/src/main/java/codeclowns/java5/labs/lab2/Api/HomeApi.java
@@ -1,10 +1,7 @@
 package codeclowns.java5.labs.lab2.Api;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,12 +10,27 @@ import java.util.Map;
 @RequestMapping("/lab2")
 public class HomeApi {
     @GetMapping("/getInformation")
-    public ResponseEntity<?> doGetInformation(@RequestParam("ten")String name,
-                                              @RequestParam("tuoi")String age){
-       Map<String,String> result = new HashMap<String,String>();
-       result.put("ten",name);
-       result.put("tuoi",age);
-       result.put("lop","Java5");
-       return ResponseEntity.ok(result);
+    public ResponseEntity<?> doGetInformation(@RequestParam("ten") String name,
+                                              @RequestParam("tuoi") String age) {
+        Map<String, String> result = new HashMap<String, String>();
+        result.put("ten", name);
+        result.put("tuoi", age);
+        result.put("lop", "Java5");
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping("/archiveLoginCredentials")
+    public ResponseEntity<?> doPostArchiveLoginCredentials(
+            @RequestBody String secretKey
+    ) {
+        var result = new HashMap<String, Object>();
+        if (secretKey.equals("Khoadz")) {
+            result.put("username", "codeclowns");
+            result.put("password", "123");
+        } else {
+            result = new HashMap<String, Object>();
+            result.put("message", "invalid token");
+        }
+        return ResponseEntity.ok(result);
     }
 }
