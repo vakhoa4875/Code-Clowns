d957e03884527042f9b9160728176e2c8c749763
GiangVoThi <giangvthps29208@fpt.edu.vn>
2024-05-20 18:50:47 +0700

    giang_changepass:create ui changepass

--- planny/src/main/java/codeclowns/planny/planny/controller/LayoutController.java
+++ planny/src/main/java/codeclowns/planny/planny/controller/LayoutController.java
@@ -2,6 +2,7 @@ package codeclowns.planny.planny.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LayoutController {
