3765e6aa94b4ca892244bfd7d1d8d321004ea17e
QuangHuy014 <huynq201104@gmail.com>
2024-05-25 20:53:36 +0700

    create-entity_commit01_huy:create-entity:user,collaborator,workspace

---
+++ planny/src/main/java/codeclowns/planny/planny/data/entity/UserE.java
@@ -0,0 +1,58 @@
+package codeclowns.planny.planny.data.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name="User")
+public class UserE {
+    @Id
+    @Column(name = "user_id")
+    private Integer id;
+
+    @Column(name = "username", length = 63, unique = true)
+    private String userName;
+
+    @Column(name = "email", length = 63, unique = true)
+    private String email;
+
+    @Column(name = "fullname", length = 63)
+    private String fullName;
+
+    @Column(name="avatar",length = 127)
+    private String avatar;
+
+    @Column(name="dob")
+    private Date dob;
+
+    @Column(name="gender")
+    private Byte gender;
+
+    @Column(name="is_private")
+    private Byte is_private;
+
+    @OneToMany (mappedBy = "User", fetch = FetchType.LAZY,cascade = {
+            CascadeType.PERSIST, CascadeType.MERGE,
+            CascadeType.DETACH, CascadeType.REFRESH
+    })
+
+    private List<Collaborator> danhSachCollaborator;
+
+
+    @OneToMany (mappedBy = "User", fetch = FetchType.LAZY,cascade = {
+            CascadeType.PERSIST, CascadeType.MERGE,
+            CascadeType.DETACH, CascadeType.REFRESH
+    })
+
+    private List<WorkSpaceE> danhSachWorkSpace;
+}
---
+++ planny/src/main/java/codeclowns/planny/planny/data/entity/WorkSpaceE.java
@@ -0,0 +1,54 @@
+package codeclowns.planny.planny.data.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.catalina.User;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name="Workspace")
+public class WorkSpaceE {
+     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "workspace_id")
+    private int workspaceId;
+
+    @Column(name = "workspace_name", nullable = false)
+    private String workspaceName;
+
+    @Column(name = "short_name", unique = true, length = 63)
+    private String shortName;
+
+    @Column(name = "website", length = 128)
+    private String website;
+
+    @Column(name = "description", length = 255)
+    private String description;
+
+    @Column(name = "is_enabled")
+    private boolean isEnabled = true;
+
+    @ManyToOne ( fetch = FetchType.LAZY,cascade = {
+            CascadeType.PERSIST, CascadeType.MERGE,
+            CascadeType.DETACH, CascadeType.REFRESH
+    })
+
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserE user;
+
+    @OneToMany (mappedBy = "Workspace", fetch = FetchType.LAZY,cascade = {
+            CascadeType.PERSIST, CascadeType.MERGE,
+            CascadeType.DETACH, CascadeType.REFRESH
+    })
+
+    private List<Collaborator> danhSachCollaborators;
+
+}
